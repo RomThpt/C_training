d5fdb6d3f23d1ea0248df77d12f6d732b06948a5
ReeFLk <92174844+ReeFLk@users.noreply.github.com>
2022-05-19 19:12:33 +0200

    random number guess

---
+++ random_number.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <windows.h>
+
+int main() 
+{   
+    const int MIN = 1;
+    const int MAX = 100;
+    srand(time(0));
+
+    int answer = (rand()% MAX)+MIN ;
+    int guess ;
+
+    printf("Guess the number : \n");
+try:
+    scanf("%d",&guess);
+
+    if (! (guess>100) && !(guess<0) )
+    {goto try;}
+
+    if (answer==guess){
+        printf("GG\n");
+        return 0;
+    }
+    else {
+        if (guess>answer){
+            printf("Too high\n");
+            goto try;
+        };
+        if (guess<answer){
+            printf("Too low\n");
+            goto try;
+        };
+    };
+
+    return 0;
+
+};
